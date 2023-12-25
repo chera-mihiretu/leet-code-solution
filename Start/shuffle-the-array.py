9acedfdcb053fa1716895b879cfa7f553b38d31e
Chera <63361469+chera-mihiretu@users.noreply.github.com>
2023-12-25 11:39:17 -0800

    Added solution for Shuffle the Array

---
+++ Start/shuffle-the-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        li = []
+        for i in range(n):
+            li.append(nums[i])
+            li.append(nums[n+i])
+        return li
