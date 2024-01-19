82b3c2e23cbb87dd1c0ace2444bd5b713dc36e02
Chera <63361469+chera-mihiretu@users.noreply.github.com>
2024-01-18 23:44:18 -0800

    Added solution for Sort Colors

--- Start/sort-colors.py
+++ Start/sort-colors.py
@@ -1,14 +1,6 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         for i in range(len(nums)):
-            min = nums[i]
-            index = i
-            for j in range(i+1, len(nums)):
-                if nums[j] < min:
-                    min = nums[j]
-                    index = j
-            if not index == i:
-                nums[i], nums[index] = nums[index], nums[i]
-
-
-        
+            while i > 0 and nums[i-1] > nums[i]:
+                nums[i-1], nums[i] = nums[i],nums[i-1]
+                i -= 1
