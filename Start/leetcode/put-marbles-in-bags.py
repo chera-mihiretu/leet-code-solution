d66958be01f0e94d14d77624615f5f860ddb7f8c
Chera <63361469+chera-mihiretu@users.noreply.github.com>
2024-02-23 18:10:45 +0300

    Add solution for Put Marbles in Bags

--- Start/leetcode/put-marbles-in-bags.py
+++ Start/leetcode/put-marbles-in-bags.py
@@ -1,13 +1,13 @@
 class Solution:
     def putMarbles(self, weights: List[int], k: int) -> int:
-        sortedList=[]
+        array = []
         for i in range(len(weights)-1):
-            sortedList.append(weights[i]+weights[i+1])
-        sortedList.sort()
-        minValue=0
-        maxValue=0
-        for i in range(k-1):
-            minValue+=sortedList[i]
-        for i in range(len(sortedList)-1,len(sortedList)-k,-1):
-            maxValue+=sortedList[i]
-        return maxValue-minValue
+            array.append(weights[i]+weights[i+1])
+        array.sort()
+
+        max_val = sum(array[len(array)-k+1:])
+        
+        min_val = sum(array[:k-1])
+        return max_val - min_val
+
+        
