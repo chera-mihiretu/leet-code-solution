38d9775953e286cbab3d8c20b921c572230fae14
Chera <63361469+chera-mihiretu@users.noreply.github.com>
2024-02-24 19:15:27 +0300

    Add solution for 132 Pattern

--- Start/leetcode/132-pattern.py
+++ Start/leetcode/132-pattern.py
@@ -2,11 +2,11 @@ class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
         stack = []
         next_large = -float('inf')
-        for i in range(len(nums)-1,-1,-1):
-            while stack and stack[-1] < nums[i]:
+        for i in nums[::-1]:
+            while stack and stack[-1] < i:
                 x = stack.pop()
                 next_large = max(next_large, x)
-            if nums[i] < next_large:
+            if i < next_large:
                 return True
-            stack.append(nums[i])
+            stack.append(i)
         return False
